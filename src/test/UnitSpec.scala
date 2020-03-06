f577b5b3965cbb3c39782b636c5aa5a2081fa940
Katie Dsouza <katie@pop-os.localdomain>
2020-03-05 21:52:07 -0500

    restore sbt tests

--- src/test/UnitSpec.scala
+++
@@ -1,7 +0,0 @@
-package test
-
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.{Inside, Inspectors, OptionValues}
-import org.scalatest.matchers.must.Matchers
-
-abstract class UnitSpec extends AnyFlatSpec with Matchers with OptionValues with Inside with Inspectors
