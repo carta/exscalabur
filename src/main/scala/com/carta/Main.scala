d66bc06fd63e75292e20e5c2103d665f3ea9acfd
carta-ksdsouza <katie.dsouza@carta.com>
2020-03-11 23:49:53 -0400

    revert changes to Main

--- src/main/scala/com/carta/Main.scala
+++ src/main/scala/com/carta/Main.scala
@@ -1,58 +1,48 @@
 package com.carta
 
-import com.carta.exscalabur.{DataCell, DataRow, Exscalabur}
+import com.carta.exscalabur.{DataRow, Exscalabur}
 
 import scala.collection.immutable
 
 object Main extends App {
   val sword = new Exscalabur(
-    "/home/katie/Desktop/exscalabur.xlsx",
-    "/home/katie/Desktop/template.yml"
+    "/Users/jacksonlo/Downloads/exscalabur.xlsx",
+    "/Users/jacksonlo/Dev/exscalabur/src/test/resources/test.yaml"
   )
 
   val row1 = DataRow.Builder()
-    .addCell(DataCell("animal", "dog"))
-    .addCell(DataCell("weight", 120))
-    .build()
-  val row2 = DataRow.Builder()
-    .addCell(DataCell("animal", "cat"))
-    .addCell(DataCell("weight", 1583812379))
+    .addCell("string", "foo")
+    .addCell("numbers", 12)
+    .addCell("dates", 1581028273)
     .build()
 
-  val name = DataCell("name", "katie")
+  val single = DataRow.Builder()
+    .addCell("company_name", "2019 Q1 HACKATHON")
+    .addCell("coop_one", "Ziyad AlYafi")
+    .addCell("coop_two", "Het Kataria")
+    .build()
 
-  //  val row1 = DataRow.Builder()
-  //    .addCell("animal", "dog")
-  //    .addCell("weight", 60)
-  //    .build()
-  //
-  //  val single = DataRow.Builder()
-  //    .addCell("company_name", "2019 Q1 HACKATHON")
-  //    .addCell("coop_one", "Ziyad AlYafi")
-  //    .addCell("coop_two", "Het Kataria")
-  //    .build()
+  val animals = List("bear", "eagle", "elephant", "bird", "snake", "pig", "dog", "cat", "penguin", "anteater");
+  val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  val foods = List("ice cream", "sushi", "curry", "sandwich", "burger", "candy", "carrots", "avocado", "la croix", "potatoes");
+  val dates = List("2019/01/01", "2019/01/02", "2019/01/03", "2019/01/04", "2019/01/05", "2019/01/06", "2019/01/07", "2019/01/08", "2019/01/09", "2019/01/10")
+  val cities = List("Toronto", "Waterloo", "Windsor", "San Francisco", "Palo Alto", "Washington", "Kitchener", "Rio de Janeiro", "SLC", "NYC")
 
-  //  val animals = List("bear", "eagle", "elephant", "bird", "snake", "pig", "dog", "cat", "penguin", "anteater");
-  //  val numbers = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-  //  val foods = List("ice cream", "sushi", "curry", "sandwich", "burger", "candy", "carrots", "avocado", "la croix", "potatoes");
-  //  val dates = List("2019/01/01", "2019/01/02", "2019/01/03", "2019/01/04", "2019/01/05", "2019/01/06", "2019/01/07", "2019/01/08", "2019/01/09", "2019/01/10")
-  //  val cities = List("Toronto", "Waterloo", "Windsor", "San Francisco", "Palo Alto", "Washington", "Kitchener", "Rio de Janeiro", "SLC", "NYC")
-
-  //  val rows: immutable.Seq[DataRow] = (0 until 10).map(i => {
-  //    DataRow.Builder()
-  //      .addCell("animals", animals(i))
-  //      .addCell("numbers", numbers(i))
-  //      .addCell("foods", foods(i))
-  //      .addCell("dates", dates(i))
-  //      .addCell("cities", cities(i))
-  //      .build()
-  //  });
+  val rows: immutable.Seq[DataRow] = (0 until 10).map(i => {
+    DataRow.Builder()
+      .addCell("animals", animals(i))
+      .addCell("numbers", numbers(i))
+      .addCell("foods", foods(i))
+      .addCell("dates", dates(i))
+      .addCell("cities", cities(i))
+      .build()
+  });
 
   sword.addTab(
-    "Sheet0",
-    "/home/katie/Desktop/template.xlsx",
-    List(name),
-    List(row1, row2)
+    "lots of formatting",
+    "/Users/jacksonlo/Downloads/demo_template.xlsx",
+    single,
+    rows.toList
   )
 
   sword.writeExcelToDisk()
