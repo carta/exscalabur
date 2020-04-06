376f29873e03d83ad52f8b02505f6ae044e114da
Katie Dsouza <ksdsouza@carta.com>
2020-04-05 20:20:00 -0400

    revert main

--- src/main/scala/com/carta/Main.scala
+++ src/main/scala/com/carta/Main.scala
@@ -7,34 +7,46 @@ import com.carta.yaml.YamlReader
 
 object Main extends App {
   override def main(args: Array[String]): Unit = {
-    val templates: List[String] = "/Users/katiedsouza/Desktop/demo.xlsx" :: Nil
+    val templates: List[String] = "/home/katie/Documents/Exscalabur Demo/template.xlsx" :: Nil
 
     val yamlReader = YamlReader()
-    val yamlData = yamlReader.parse(new File("/Users/katiedsouza/Desktop/demo.yaml"))
+    val yamlData = yamlReader.parse(new File("/home/katie/Documents/Exscalabur Demo/template.yml"))
 
     val sword = Exscalabur(
       templates,
-      "/Users/katiedsouza/Desktop/demo.out.xlsx",
+      "/home/katie/Documents/Exscalabur Demo/out/exscalabur.xlsx",
       yamlData,
       100
     )
 
-    val staticData1 = List(
-      DataCell("string_field", "katie"),
-      DataCell("long_field", 1234),
-      DataCell("double_field", 1235.1),
+    val repeatedData: List[DataRow] = List(
+      DataRow.Builder().addCell("animal", "monkey").build(),
+      DataRow.Builder().addCell("animal", "horse").build(),
+      DataRow.Builder().addCell("animal", "cow").build(),
     )
-    val staticData2 = List(
-      DataCell("string_field2", "katie2"),
-      DataCell("long_field2", 21234),
-      DataCell("double_field2", 21235.1),
+
+    val repeatedData2 = List(
+      DataRow.Builder().addCell("animal", "dog").build(),
+      DataRow.Builder().addCell("animal", "cat").build(),
     )
+
+    val repeatedData3 = List(
+      DataRow.Builder().addCell("element", "hydrogen").build(),
+      DataRow.Builder().addCell("element", "helium").build(),
+      DataRow.Builder().addCell("element", "lithium").build()
+    )
+
+
     val sheetWriter = sword.getAppendOnlySheetWriter("Sheet1")
 
     val dataProvider: Iterator[(List[DataCell], List[DataRow])] = Iterator(
-      (staticData1, List.empty),
-      (staticData2, List.empty),
+      (List.empty, repeatedData),
+      (List.empty, repeatedData2),
+      (List.empty, repeatedData2),
+      (List(DataCell("name", "katie")), List.empty),
+      (List.empty, repeatedData3)
     )
+
 
     sheetWriter.writeData(dataProvider)
 
